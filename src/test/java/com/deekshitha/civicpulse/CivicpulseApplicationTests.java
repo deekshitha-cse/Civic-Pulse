142730960ba6267957e36f7e3a9b39c99ae9b758
Deekshitha <dxshitha@gmail.com>
2026-07-29 23:02:20 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/deekshitha/civicpulse/CivicpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deekshitha.civicpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
